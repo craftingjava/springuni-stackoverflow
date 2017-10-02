12e33b5d6f6ca893b81fee9d2f0b6f12f630cde1
Laszlo Csontos <github@laszlocsontos.com>
2017-10-02 15:08:51 +0200

    Question #46521912: Adjustments

--- question-46521912/src/main/java/com/springuni/question46521912/ClassController.java
+++ question-46521912/src/main/java/com/springuni/question46521912/ClassController.java
@@ -1,9 +1,9 @@
 package com.springuni.question46521912;
 
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,16 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ClassController {
 
-    @RequestMapping("/application/index.jsp")
+    @RequestMapping("/application/index.do")
     public String index(HttpServletRequest request, ModelMap modelMap) {
         // This one can handle any rest URIs, e.g: localhost:8080/application/abc, localhost:8080/application/abc/12/asdasd/3123,...
         modelMap.put("requestURI", request.getRequestURI());
         return "index";
     }
 
-    @RequestMapping("/application/{arg1}/{arg2}")
-    public Integer handle(@PathVariable int arg1, @PathVariable int arg2) {
-        return arg1 + arg2;
+    @RequestMapping("/application/**")
+    public ResponseEntity handle(HttpServletRequest request) {
+        return ResponseEntity.ok(request.getRequestURI());
     }
 
 }
